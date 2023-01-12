5ac384cbe36051fd170ea90c3d39027e2b5feba4
Victor Villazón <victorzn23@gmail.com>
2023-01-12 11:28:16 -0500

    test: it will remove a match from the scoreboard (passed)

--- src/ScoreBoard.test.tsx
+++ src/ScoreBoard.test.tsx
@@ -1,35 +1,66 @@
 
 import { cleanup, render, screen } from '@testing-library/react'
 import { afterEach, describe, expect, it } from 'vitest'
+import userEvent from '@testing-library/user-event'
+
+import React, { useState } from 'react'
 
 const ScoreBoard = (): JSX.Element => {
+  const DEFAULT_FORM = {
+    homeTeamName: '',
+    homeTeamScore: '0',
+    awayTeamName: '',
+    awayTeamScore: '0'
+  }
+
+  const [form, setForm] = useState(DEFAULT_FORM)
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+
+    setForm({ ...form, [name]: value })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    setForm(DEFAULT_FORM)
+  }
+
   return (
     <>
       <h1>Score Board</h1>
       <h2>Start Game</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type='text'
           name='homeTeamName'
           placeholder='Home Team Name'
+          value={form.homeTeamName}
+          onChange={handleChange}
         />
         <input
           type='text'
           name='homeTeamScore'
           placeholder='Home Team Score'
-          value='0'
+          value={form.homeTeamScore}
+          onChange={handleChange}
         />
         <input
           type='text'
           name='awayTeamName'
           placeholder='Away Team Name'
+          value={form.awayTeamName}
+          onChange={handleChange}
         />
         <input
           type='text'
           name='awayTeamScore'
           placeholder='Away Team Score'
-          value='0'
+          value={form.awayTeamScore}
+          onChange={handleChange}
         />
+        <button>Finish Game</button>
       </form>
     </>
   )
@@ -72,6 +103,35 @@ describe('<ScoreBoard />', () => {
       const awayTeamScore: HTMLInputElement = screen.getByPlaceholderText('Away Team Score')
 
       expect(homeTeamScore.value).toBe('0')
+      expect(awayTeamScore.value).toBe('0')
+    })
+  })
+
+  describe('End a game', () => {
+    it('will remove a match from the scoreboard', async () => {
+      const user = userEvent.setup()
+
+      render(<ScoreBoard />)
+
+      const homeTeamName: HTMLInputElement = screen.getByPlaceholderText('Home Team Name')
+      const homeTeamScore: HTMLInputElement = screen.getByPlaceholderText('Home Team Score')
+      const awayTeamName: HTMLInputElement = screen.getByPlaceholderText('Away Team Name')
+      const awayTeamScore: HTMLInputElement = screen.getByPlaceholderText('Away Team Score')
+
+      const finishGameButton: HTMLButtonElement = screen.getByText('Finish Game')
+
+      homeTeamName.defaultValue = 'Peru'
+      homeTeamScore.defaultValue = '6'
+
+      awayTeamName.defaultValue = 'Nigeria'
+      awayTeamScore.defaultValue = '3'
+
+      await user.click(finishGameButton)
+
+      expect(homeTeamName.value).toBe('')
+      expect(homeTeamScore.value).toBe('0')
+
+      expect(awayTeamName.value).toBe('')
       expect(awayTeamScore.value).toBe('0')
     })
   })
