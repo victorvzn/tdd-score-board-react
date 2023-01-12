eb7e518b2cb9014a09a60ce68b5f9298aa2682a7
Victor Villazón <victorzn23@gmail.com>
2023-01-12 10:37:17 -0500

    test: it should capture home team and away team names and scores

--- src/ScoreBoard.test.tsx
+++ src/ScoreBoard.test.tsx
@@ -7,6 +7,28 @@ const ScoreBoard = (): JSX.Element => {
     <>
       <h1>Score Board</h1>
       <h2>Start Game</h2>
+      <form>
+        <input
+          type='text'
+          name='homeTeamName'
+          placeholder='Home Team Name'
+        />
+        <input
+          type='text'
+          name='homeTeamScore'
+          placeholder='Home Team Score'
+        />
+        <input
+          type='text'
+          name='awayTeamName'
+          placeholder='Away Team Name'
+        />
+        <input
+          type='text'
+          name='awayTeamScore'
+          placeholder='Away Team Score'
+        />
+      </form>
     </>
   )
 }
@@ -28,5 +50,17 @@ describe('<ScoreBoard />', () => {
     render(<ScoreBoard />)
 
     screen.getByText('Start Game')
+  })
+
+  describe('Start a game', () => {
+    it('should capture home team and away team names and scores', () => {
+      render(<ScoreBoard />)
+
+      screen.getByPlaceholderText('Home Team Name')
+      screen.getByPlaceholderText('Home Team Score')
+
+      screen.getByPlaceholderText('Away Team Name')
+      screen.getByPlaceholderText('Away Team Score')
+    })
   })
 })
