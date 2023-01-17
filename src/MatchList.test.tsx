b35478ba6eae534d3c0b58ce797c0929f858a53b
Victor Villazón <victorzn23@gmail.com>
2023-01-17 06:19:13 -0500

    test: it should hide home team score and way team score inputs with scores value after save button clicked (failed)

--- src/MatchList.test.tsx
+++ src/MatchList.test.tsx
@@ -120,3 +120,43 @@ describe('<MatchList />', () => {
 
     expect(screen.getByText('Save Scores')).toBeInTheDocument()
   })
+
+  it('should hide home team score and way team score inputs with scores value after save button clicked', async () => {
+    const user = userEvent.setup()
+
+    const mockMatches = [
+      {
+        id: 'uuid-1',
+        homeTeamName: 'Team P',
+        homeTeamScore: 5,
+        awayTeamName: 'Team S',
+        awayTeamScore: 3
+      },
+      {
+        id: 'uuid-2',
+        homeTeamName: 'Team X',
+        homeTeamScore: 5,
+        awayTeamName: 'Team W',
+        awayTeamScore: 3
+      }
+    ]
+
+    render(<MatchList matches={mockMatches} />)
+
+    // const updateScoresButton: HTMLButtonElement = screen.getByText('Update Scores')
+    const row = screen.getByRole('row', { name: /Team X 5 Team W 3/i })
+    const updateScoresButton = within(row).getByText('Update Scores')
+
+    await user.click(updateScoresButton)
+
+    expect(within(row).getByPlaceholderText('HT Score')).toBeInTheDocument()
+    expect(within(row).getByPlaceholderText('AT Score')).toBeInTheDocument()
+    expect(within(row).getByText('Save Scores')).toBeInTheDocument()
+
+    await user.click(within(row).getByText('Save Scores'))
+
+    expect(within(row).queryByPlaceholderText('HT Score')).not.toBeInTheDocument()
+    expect(within(row).queryByPlaceholderText('AT Score')).not.toBeInTheDocument()
+    expect(updateScoresButton).toBeInTheDocument()
+  })
+
