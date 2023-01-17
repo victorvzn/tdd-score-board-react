66a196268c95fe7e061399329a86f33a488ef734
Victor Villazón <victorzn23@gmail.com>
2023-01-17 13:18:54 -0500

    feat: config npm publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 /// <reference types="vitest" />
 
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/Scoreboard.tsx'),
+      name: 'Scoreboard',
+      fileName: (format) => `scoreboard.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  },
   plugins: [react()],
   test: {
     globals: true,
