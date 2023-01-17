2acdbd8a6faf8946983c28c270a9a339a2cef215
Victor Villazón <victorzn23@gmail.com>
2023-01-17 06:07:22 -0500

    test: it should toggle home team score and way team score inputs only once after update button clicked (passed)

--- src/MatchList.test.tsx
+++ src/MatchList.test.tsx
@@ -1,8 +1,8 @@
 
 import React from 'react'
 
-import { cleanup, render, screen } from '@testing-library/react'
-import { afterEach, describe, it, expect, vi } from 'vitest'
+import { cleanup, render, screen, within } from '@testing-library/react'
+import { afterEach, describe, it, expect } from 'vitest'
 import userEvent from '@testing-library/user-event'
 
 import { MatchList } from './MatchList'
@@ -76,6 +76,44 @@ describe('<MatchList />', () => {
     expect(screen.queryByPlaceholderText('AT Score')).not.toBeInTheDocument()
 
     await user.click(updateScoresButton)
+
+    expect(screen.getByPlaceholderText('HT Score')).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('AT Score')).toBeInTheDocument()
+
+    expect(screen.getByText('Save Scores')).toBeInTheDocument()
+  })
+
+  it('should toggle home team score and way team score inputs only once after update button clicked', async () => {
+    const user = userEvent.setup()
+
+    const mockMatches = [
+      {
+        id: 'uuid-1',
+        homeTeamName: 'Team P',
+        homeTeamScore: 5,
+        awayTeamName: 'Team S',
+        awayTeamScore: 3
+      },
+      {
+        id: 'uuid-2',
+        homeTeamName: 'Team X',
+        homeTeamScore: 5,
+        awayTeamName: 'Team W',
+        awayTeamScore: 3
+      }
+    ]
+
+    render(<MatchList matches={mockMatches} />)
+
+    // const updateScoresButton: HTMLButtonElement = screen.getByText('Update Scores')
+    const row = screen.getByRole('row', { name: /Team X 5 Team W 3/i })
+    const updateScoresButton = within(row).getByText('Update Scores')
+
+    expect(screen.queryByPlaceholderText('HT Score')).not.toBeInTheDocument()
+    expect(screen.queryByPlaceholderText('AT Score')).not.toBeInTheDocument()
+
+    await user.click(updateScoresButton)
+    screen.debug()
 
     expect(screen.getByPlaceholderText('HT Score')).toBeInTheDocument()
     expect(screen.getByPlaceholderText('AT Score')).toBeInTheDocument()
