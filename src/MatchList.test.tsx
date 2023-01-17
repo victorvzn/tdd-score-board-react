ec9d355d27e50d4419bdfe5b68657b0bf0256485
Victor Villazón <victorzn23@gmail.com>
2023-01-17 07:48:15 -0500

    test: it should save home team score and way team score inputs with score values after save button clicked (failed)

--- src/MatchList.test.tsx
+++ src/MatchList.test.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 
 import { cleanup, render, screen, within } from '@testing-library/react'
-import { afterEach, describe, it, expect } from 'vitest'
+import { afterEach, describe, it, expect, vi } from 'vitest'
 import userEvent from '@testing-library/user-event'
 
 import { MatchList } from './MatchList'
@@ -160,3 +160,51 @@ describe('<MatchList />', () => {
     expect(updateScoresButton).toBeInTheDocument()
   })
 
+  it('should save home team score and way team score inputs with score values after save button clicked', async () => {
+    const user = userEvent.setup()
+    const mockHandleSaveMatch = vi.fn()
+
+    const mockMatches = [
+      {
+        id: 'uuid-1',
+        homeTeamName: 'Team P',
+        homeTeamScore: 5,
+        awayTeamName: 'Team S',
+        awayTeamScore: 3
+      },
+      {
+        id: 'uuid-2',
+        homeTeamName: 'Team X',
+        homeTeamScore: 5,
+        awayTeamName: 'Team W',
+        awayTeamScore: 3
+      }
+    ]
+
+    render(<MatchList matches={mockMatches} onSaveMatch={mockHandleSaveMatch} />)
+
+    // const updateScoresButton: HTMLButtonElement = screen.getByText('Update Scores')
+    const row = screen.getByRole('row', { name: /Team X 5 Team W 3/i })
+    const updateScoresButton = within(row).getByText('Update Scores')
+
+    await user.click(updateScoresButton)
+
+    await user.type(within(row).getByPlaceholderText('HT Score'), '{backspace}88')
+
+    await user.type(within(row).getByPlaceholderText('AT Score'), '{backspace}99')
+
+    await user.click(within(row).getByText('Save Scores'))
+
+    expect(mockHandleSaveMatch).toHaveBeenCalledOnce()
+
+    expect(mockHandleSaveMatch).toHaveBeenCalledWith({
+      id: 'uuid-2',
+      homeTeamName: 'Team X',
+      homeTeamScore: 88,
+      awayTeamName: 'Team W',
+      awayTeamScore: 99
+    })
+
+    // TODO: ADD E2E test
+  })
+})
